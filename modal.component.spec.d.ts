ec7197890cafbbca868c0e54bfcc0b5fe4259627
MadUser <mr_van_halen@hotmail.com>
2016-07-09 21:09:46 +0300

    Delete modal.component.spec.d.ts

--- modal.component.spec.d.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentResolver, ComponentFactory } from "@angular/core";
-import { Type } from "@angular/http/esm/src/facade/lang";
-export declare class DummyComponentResolver implements ComponentResolver {
-    private _resolver;
-    constructor(_resolver: ComponentResolver);
-    resolveComponent(componentType: string | Type): Promise<ComponentFactory<any>>;
-    clearCache(): void;
-}
