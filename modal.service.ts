e201c7e0a4dc3e0ab9d8faa149026d47e56c5550
MadUser <mr_van_halen@hotmail.com>
2016-07-14 17:53:01 +0300

    Update modal.service.ts

--- modal.service.ts
+++ modal.service.ts
@@ -1,16 +1,25 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
-export class ModalService {
+export class ModalService implements OnDestroy {
     // Observable source
     private responseSource = new Subject<any>();
-
-    // Observable stream
-    response$ = this.responseSource.asObservable();
 
     // service message command
     response(response: any) {
         this.responseSource.next(response);
+    }
+
+    getObservable(): Observable<any> {
+        return this.responseSource.asObservable();
+    }
+
+    ngOnDestroy() {
+        if (this.responseSource) {
+            this.responseSource.complete();
+            this.responseSource.unsubscribe();
+        }
     }
 }
